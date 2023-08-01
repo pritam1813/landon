30b64b147358705eed38d4ad7932e910871d12d0
Pritam Dhara <pritam1813@gmail.com>
2023-08-01 21:06:31 +0530

    Organized the component directory structure

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Welcome from "./Welcome";
+import HotelInfo from "./HotelInfo";
+
+function Main() {
+  return (
+    <main id="wrapper">
+      <Welcome />
+
+      <HotelInfo />
+    </main>
+  );
+}
+
+export default Main;
