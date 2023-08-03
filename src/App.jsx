3a5d7b286aa846bc88cf451799fd351f1a7b9588
Pritam Dhara <pritam1813@gmail.com>
2023-08-03 20:36:24 +0530

    Modified Css and Changed file Location

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+import "./App.css";
 
 class App extends React.Component {
   render() {
